ac558367b9864be79865fc39ad1abd0e0eaa4f0a
abirremadi <abirremadi4@gmail.com>
2021-07-13 00:31:53 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config({ path: './config.env' })
+// routes
+app.use('/U', require('./Routers/UserRouter'))
+
+// connect database
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+     useUnifiedTopology: true
+    }).then(res => console.log('database connected'))
+    .catch(err => console.log(err));
+// create server
+const PORT= process.env.PORT || 5000;
+app.listen(PORT,(err)=>err?console.log(err):console.log('server is running on port',PORT))
